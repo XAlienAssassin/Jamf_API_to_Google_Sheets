a0f28acdd4ebc68986be5b27247f714e47c434b9
Orion Medina <31485978+XAlienAssassin@users.noreply.github.com>
2023-09-18 09:05:01 -0400

    added preschool and kindergarten
    added a section to get the data from the mobile device groups of Kindergarten and preK.

--- Jamf_API_to_Google_Sheets.js
+++ Jamf_API_to_Google_Sheets.js
@@ -23,6 +23,8 @@ function uiChanges() {
       .addItem('Class of 2031','classOf2031')
       .addItem('Class of 2032','classOf2032')
       .addItem('Class of 2033','classOf2033')
+      .addItem('PreK', 'preK')
+      .addItem('Kindergarten', 'kinderGarten')
       //.deleteItem('Class of 2024')
       //.addItem('Class of 2034','classOf2034')
       .addToUi();
@@ -642,6 +644,130 @@ function classOf2033() {
     var department = computer.getChildText("Department");
 
     rowData.push([fullName, computerName, serialNumber, model, operatingSystem, lastCheckIn, department]);
+  });
+
+  // Write the data starting below the headers
+  sheet.getRange(2, 1, rowData.length, rowData[0].length).setValues(rowData);
+
+  Logger.log("API Response parsed and data written to Google Sheets.");
+}
+
+
+function preK() {
+  var username = " "; // Replace with your Jamf username
+  var password = " "; // Replace with your Jamf password
+  var jamfURL = " "; // Replace with your Jamf instance URL
+  var advancedSearchID = "71"; // Replace with the ID of your advanced mobile search
+
+  var headers = {
+    "Authorization": "Basic " + Utilities.base64Encode(username + ":" + password)
+  };
+  
+  var options = {
+    "method": "get",
+    "headers": headers
+  };
+
+  var response = UrlFetchApp.fetch(jamfURL + "/JSSResource/advancedmobiledevicesearches/id/" + advancedSearchID, options);
+  var data = response.getContentText();
+
+  // Parse the XML response
+  var xmlDoc = XmlService.parse(data);
+  var mobileElements = xmlDoc.getRootElement().getChild("mobile_devices").getChildren("mobile_device");
+
+  // Get the active sheet in the currently open spreadsheet
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getActiveSheet();
+
+  // Find the last row with data in column A
+  var lastRowWithData = sheet.getLastRow();
+
+  // If there is data below the headers, clear that specific range
+  if (lastRowWithData > 1) {
+    sheet.getRange(2, 1, lastRowWithData - 1, 7).clear();
+  }
+
+  // Write headers to the sheet
+  var headers = ["Full Name", "Display Name", "Serial Number", "Model", "iOS Version", "Last Inventory Update", "Username"];
+  sheet.getRange(1, 1, 1, headers.length).setValues([headers]);
+
+  // Iterate through the XML data and write it to the sheet starting below the headers
+  var rowData = [];
+  mobileElements.forEach(function(mobile) {
+    //changes the font and the size before the data populates into the google
+    changeFontAndSize();
+
+    var fullName = mobile.getChildText("Full_Name");
+    var displayName = mobile.getChildText("Display_Name");
+    var serialNumber = mobile.getChildText("Serial_Number");
+    var model = mobile.getChildText("Model");
+    var iOSVersion = mobile.getChildText("iOS_Version");
+    var lastInventoryUpdate = mobile.getChildText("Last_Inventory_Update");
+    var userName = mobile.getChildText("Username");
+
+    rowData.push([fullName, displayName, serialNumber, model, iOSVersion, lastInventoryUpdate, userName]);
+  });
+
+  // Write the data starting below the headers
+  sheet.getRange(2, 1, rowData.length, rowData[0].length).setValues(rowData);
+
+  Logger.log("API Response parsed and data written to Google Sheets.");
+}
+
+
+function kinderGarten() {
+  var username = " "; // Replace with your Jamf username
+  var password = " "; // Replace with your Jamf password
+  var jamfURL = " "; // Replace with your Jamf instance URL
+  var advancedSearchID = "106"; // Replace with the ID of your advanced mobile search
+
+  var headers = {
+    "Authorization": "Basic " + Utilities.base64Encode(username + ":" + password)
+  };
+  
+  var options = {
+    "method": "get",
+    "headers": headers
+  };
+
+  var response = UrlFetchApp.fetch(jamfURL + "/JSSResource/advancedmobiledevicesearches/id/" + advancedSearchID, options);
+  var data = response.getContentText();
+
+  // Parse the XML response
+  var xmlDoc = XmlService.parse(data);
+  var mobileElements = xmlDoc.getRootElement().getChild("mobile_devices").getChildren("mobile_device");
+
+  // Get the active sheet in the currently open spreadsheet
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getActiveSheet();
+
+  // Find the last row with data in column A
+  var lastRowWithData = sheet.getLastRow();
+
+  // If there is data below the headers, clear that specific range
+  if (lastRowWithData > 1) {
+    sheet.getRange(2, 1, lastRowWithData - 1, 7).clear();
+  }
+
+  // Write headers to the sheet
+  var headers = ["Full Name", "Display Name", "Serial Number", "Model", "iOS Version", "Last Inventory Update", "Username"];
+  sheet.getRange(1, 1, 1, headers.length).setValues([headers]);
+
+  // Iterate through the XML data and write it to the sheet starting below the headers
+  var rowData = [];
+  mobileElements.forEach(function(mobile) {
+    //changes the font and the size before the data populates into the google
+    changeFontAndSize();
+
+    var fullName = mobile.getChildText("Full_Name");
+    var displayName = mobile.getChildText("Display_Name");
+    var serialNumber = mobile.getChildText("Serial_Number");
+    var model = mobile.getChildText("Model");
+    var iOSVersion = mobile.getChildText("iOS_Version");
+    var lastInventoryUpdate = mobile.getChildText("Last_Inventory_Update");
+    var userName = mobile.getChildText("Username");
+
+    rowData.push([fullName, displayName, serialNumber, model, iOSVersion, lastInventoryUpdate, userName]);
   });
 
   // Write the data starting below the headers
